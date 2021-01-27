31a9a065e23e5dfe1d2503075086fbf3bc3d296b
mgxmajewski <mgxmajewski@gmail.com>
2021-01-27 15:34:14 +0000

    Add nested loop to print '#' in two dimensions

--- mario.py
+++ mario.py
@@ -6,3 +6,8 @@ while True:
     user_input = get_int("Give number from 1 to 8:")
     if user_input > 1 and user_input < 8:
         break
+
+for i in range(user_input):
+    for j in range(user_input):
+        print("#", end="")
+    print()
